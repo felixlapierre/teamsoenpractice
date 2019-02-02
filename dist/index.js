e655655e88e19ac269a8e3bf19a26581cf817a1d
Felix <felix.lapierre1@gmail.com>
2019-02-02 17:01:57 -0500

    Added basic get request

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const userRoute_1 = require("./routes/userRoute");
 const app = express_1.default();
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+const routes = new userRoute_1.Routes();
+routes.routes(app);
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// });
 app.listen(8000, () => {
     // tslint:disable-next-line:no-console
     console.log("Started server on port 8000");
